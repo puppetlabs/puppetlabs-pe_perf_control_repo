684fec256924ddba4de5ba0d572b85a05758e93b
John Duarte <john.duarte@puppet.com>
2019-04-24 15:53:03 -0700

    (SLV-381) Update influxdb profile manifest
    This commit updates the profile manifest for influxdb to make it
    compliant with the git version of the golja-influxdb module.

--- site/profile/manifests/influxdb/basic.pp
+++ site/profile/manifests/influxdb/basic.pp
@@ -1,30 +1,34 @@
 class profile::influxdb::basic {
 
   $graphite_config = {
+    'default' => {
       'enabled'           => true, # not default
+    }
   }
 
-  $udp_options = [
-    { 'enabled'       => true,
+  $udp_config = {
+    'udp1' => {
+      'enabled'       => true,
       'bind-address'  => ':8089',
       'database'      => 'udp_db1',
       'batch-size'    => 10000,
       'batch-timeout' => '1s',
       'batch-pending' => 5,
     },
-    { 'enabled'       => true,
+    'udp2' => {
+      'enabled'       => true,
       'bind-address'  => ':8090',
       'database'      => 'udp_db2',
       'batch-size'    => 10000,
       'batch-timeout' => '1s',
       'batch-pending' => 5,
     },
-]
+}
 
-  class { 'influxdb::server':
+  class { 'influxdb':
     manage_repos     => true,
-    graphite_options => $graphite_config,
-    udp_options      => $udp_options,
+    graphite_config  => $graphite_config,
+    udp_config       => $udp_config,
   }
 
 }
